7f6e4fdc46442162421eaa55e258e2c26b34b9dd
Henry781029 <61300603+Henry781029@users.noreply.github.com>
2021-07-15 14:37:46 -0500

    organizando buscar plato

--- buscar_plato.php
+++ buscar_plato.php
@@ -2,6 +2,27 @@
 
 <?php session_start();
 
+    if(isset($_SESSION['idPlato'])){
+        $idplato = $_SESSION['idPlato'];
+    }else{
+        $idplato = '';
+    }
+    if(isset($_SESSION['nombre'])){
+        $nombre = $_SESSION['nombre'];
+    }else{
+        $nombre = '';
+    }
+    if(isset($_SESSION['descripcion'])){
+        $descripcion = $_SESSION['descripcion'];
+    }else{
+        $descripcion = '';
+    }
+    if(isset($_SESSION['precio'])){
+        $precio = $_SESSION['precio'];
+    }else{
+        $precio = '';
+    }
+
 ?>
 
 <div class="container-p-4">
@@ -12,24 +33,44 @@
             <div class="card card-body">
                 <form action="find_plato.php" method = "POST">
                     <div class="form-group">
-                        <input type="text" name = "idPlato" value="<?php echo $_SESSION['idPlato'];?>"  class = "form-control" placeholder = "IdPlato" autofocus>
+                        <input type="text" name = "idPlato" value="<?php echo $idplato;?>"  class = "form-control" placeholder = "IdPlato" autofocus>
                         <br>
-                        <input type="submit" class = "btn btn-primary btn-block" name = "buscar_plato" value = "Buscar" >
+                        <span>
+                            <?php 
+                            if ($idplato == '')
+                            {
+                                echo "id del plato NO existe!!";
+                            }
+                            else{
+                                echo '';
+                            } 
+                            ?>
+                        </span>
+                        <br>
+                        <br>
+                        <input type="submit" class = "btn btn-primary btn-block" name = "buscar_plato" value = "Buscar" >  
                     </div>
                     <br>
                     <div class="form-group">
-                        <input type="text" name = "nombre" value="<?php echo $_SESSION['nombre'];?>"  class = "form-control" placeholder = "Nombre Plato" autofocus>
+                        <input type="text" name = "nombre" value="<?php echo $nombre;?>"  class = "form-control" placeholder = "Nombre Plato" autofocus>
                     </div>
                     <div class="form-group">
-                        <input type="text" name = "descripcion" value="<?php echo $_SESSION['descripcion'];?>"  class = "form-control" placeholder = "descripcion">
+                        <input type="text" name = "descripcion" value="<?php echo $descripcion;?>"  class = "form-control" placeholder = "descripcion">
                     </div>
                     <div class="form-group">
-                        <input type="text" name = "precio" value="<?php echo $_SESSION['precio'];?>"  class = "form-control" placeholder = "precio">
+                        <input type="text" name = "precio" value="<?php echo $precio;?>"  class = "form-control" placeholder = "precio">
                     </div>
                      <br>  
-                    <form action="editar_plato.php"  method = "POST">
-                    <input type="submit" class = "btn btn-warning btn-block" name = "editar_plato" value = "Actualizar" >
-                    </form>
+                    
+                </form>
+                <form action="editar_plato.php"  method = "POST">
+                    <input type="submit" class = "btn btn-warning" style ="float:left" name = "editar_plato" value = "Actualizar" >
+                    <i class="fas fa-marker"></i>
+                </form>
+                    <br>
+                <form action="borrar_plato.php"  method = "POST">
+                    <input type="submit" class = "btn btn-danger" style = "float:left" name = "borrar_plato" value = "Eliminar" >
+                    <i class="fas fa-trash-alt"></i>
                 </form>
             </div>         
 
